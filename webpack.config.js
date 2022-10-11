55ac2e0af2a815305594b1e185a2193e513a3396
Dmitry Petrouk <30505853+dpetrouk@users.noreply.github.com>
2022-10-11 18:59:40 +0300

    setup environment

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  entry: './src/index.js',
+  // output: {
+  //   path: path.join(__dirname, 'dist', 'public'),
+  //   publicPath: '/assets/',
+  // },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: file => (
+          /node_modules/.test(file) &&
+          !/\.vue\.js/.test(file)
+        ),
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      '__VUE_OPTIONS_API__': true,
+      '__VUE_PROD_DEVTOOLS__': false
+    }),
+    new VueLoaderPlugin()
+  ]
+}
